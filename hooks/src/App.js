601d32bdadd6a888dfa313807fc8e4bb26b67f8a
Masatoshi <msts.oo0131@gmail.com>
2021-03-16 22:05:17 +0900

    spread.operator_learned

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import './App.css';
 import ClassCounter from "./components/ClassCounter";
-import HookCounter from "./components/HookCounter";
+// import HookCounter from "./components/HookCounter";
+import HookCounterThree from "./components/HookCounterThree";
+import HookCounterFour from "./components/HookCounterFour";
 
 function App() {
   return (
     <div className="App">
       {/*<ClassCounter/>*/}
-      <HookCounter/>
+      {/*<HookCounter/>*/}
+      {/*<HookCounterThree/>*/}
+      <HookCounterFour/>
     </div>
   );
 }
