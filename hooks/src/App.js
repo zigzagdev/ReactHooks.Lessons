db97e67a2e23374fe64b9c4dde1c0c247e8ab7b0
Masatoshi <msts.oo0131@gmail.com>
2021-03-22 22:05:48 +0900

    first_point_cleared

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,15 +1,21 @@
 import React from 'react'
-import './App.css';
-import ClassCounter from "./components/ClassCounter";
-import HookCounter from "./components/HookCounter";
+import './App.css'
+import ClassCounterOne from "./components/ClassCounterOne";
+import HookCounterOne from "./components/HookCounterOne";
+import ClassMouse from "./components/ClassMouse";
+import HookMouse from "./components/HookMouse";
+import MouseContainer from "./components/MouseContainer";
 
 function App() {
-  return (
-    <div className="App">
-      {/*<ClassCounter/>*/}
-      <HookCounter/>
-    </div>
-  );
+ return (
+  <div className='App'>
+    {/*<ClassCounterOne/>*/}
+    {/*<HookCounterOne/>*/}
+    {/*<ClassMouse/>*/}
+    {/*<HookMouse/>*/}
+    <MouseContainer/>
+  </div>
+ )
 }
 
-export default App;
+export default App
