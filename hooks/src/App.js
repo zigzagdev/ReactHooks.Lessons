a0d3bef9482a29b901d419e1ddf96d74c4f31c55
Masatoshi <msts.oo0131@gmail.com>
2021-03-28 20:08:54 +0900

    useEffect_learned

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -5,6 +5,8 @@ import HookCounterOne from "./components/HookCounterOne";
 import ClassMouse from "./components/ClassMouse";
 import HookMouse from "./components/HookMouse";
 import MouseContainer from "./components/MouseContainer";
+import IntervalClassCounter from "./components/IntervalClassCounter";
+import IntervalHookCounter from "./components/IntervalHookCounter";
 
 function App() {
  return (
@@ -13,7 +15,9 @@ function App() {
     {/*<HookCounterOne/>*/}
     {/*<ClassMouse/>*/}
     {/*<HookMouse/>*/}
-    <MouseContainer/>
+    {/*<MouseContainer/>*/}
+    <IntervalClassCounter/>
+    <IntervalHookCounter/>
   </div>
  )
 }
