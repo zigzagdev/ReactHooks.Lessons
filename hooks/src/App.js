d93cb19068f95959054e2448cc199e9ccb2a6b95
Masatoshi <msts.oo0131@gmail.com>
2021-09-24 15:25:51 +0900

    Unnecessary_file_delete

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,21 +1,11 @@
 import React from 'react'
 import './App.css'
-import ClassCounterOne from "./components/ClassCounterOne";
-import HookCounterOne from "./components/HookCounterOne";
-import ClassMouse from "./components/ClassMouse";
-import HookMouse from "./components/HookMouse";
-import MouseContainer from "./components/MouseContainer";
 import IntervalClassCounter from "./components/IntervalClassCounter";
 import IntervalHookCounter from "./components/IntervalHookCounter";
 
 function App() {
  return (
   <div className='App'>
-    {/*<ClassCounterOne/>*/}
-    {/*<HookCounterOne/>*/}
-    {/*<ClassMouse/>*/}
-    {/*<HookMouse/>*/}
-    {/*<MouseContainer/>*/}
     <IntervalClassCounter/>
     <IntervalHookCounter/>
   </div>
