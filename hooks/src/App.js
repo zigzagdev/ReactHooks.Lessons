7b684edc6d3e4e488e3539a91acfb559f38f2c12
Masatoshi <msts.oo0131@gmail.com>
2021-09-24 15:22:26 +0900

    Unnecessary_file_delete

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,13 +1,5 @@
 import React from 'react'
 import './App.css'
-import ClassCounterOne from "./components/ClassCounterOne";
-import HookCounterOne from "./components/HookCounterOne";
-import ClassMouse from "./components/ClassMouse";
-import HookMouse from "./components/HookMouse";
-import MouseContainer from "./components/MouseContainer";
-import IntervalClassCounter from "./components/IntervalClassCounter";
-import IntervalHookCounter from "./components/IntervalHookCounter";
-import DataFetching from "./components/DataFetching";
 import ComponentC from "./components/ComponentC";
 export  const UserContext = React.createContext()
 export  const ChannelContext = React.createContext()
@@ -16,14 +8,6 @@ export  const ChannelContext = React.createContext()
 function App() {
  return (
   <div className='App'>
-    {/*<ClassCounterOne/>*/}
-    {/*<HookCounterOne/>*/}
-    {/*<ClassMouse/>*/}
-    {/*<HookMouse/>*/}
-    {/*<MouseContainer/>*/}
-    {/*<IntervalClassCounter/>*/}
-    {/*<IntervalHookCounter/>*/}
-    {/*<DataFetching/>*/}
     <UserContext.Provider value= {' Matthew '}>
         <ChannelContext.Provider value= {'fuck'}>
             <ComponentC/>
